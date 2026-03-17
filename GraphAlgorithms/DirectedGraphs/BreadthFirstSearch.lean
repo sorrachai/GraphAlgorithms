60bcba229fe21fd6883a5ca48962ac30378344eb
nnhjy <43530784+nnhjy@users.noreply.github.com>
2026-03-17 19:04:42 +0100

    init bfs

---
+++ GraphAlgorithms/DirectedGraphs/BreadthFirstSearch.lean
@@ -0,0 +1,20 @@
+import Mathlib.Tactic
+import Mathlib.Order.WithBot
+import Mathlib.Data.Sym.Sym2
+import Mathlib.Data.Finset.Basic
+
+import GraphAlgorithms.DirectedGraphs.SimpleDiGraphs
+import GraphAlgorithms.DirectedGraphs.Walk
+
+-- Breadth-first Search
+-- Authors: Huang, JiangYi
+
+set_option tactic.hygienic false
+variable {α : Type*} [DecidableEq α]
+
+open SimpleDiGraph
+open Walk
+
+namespace BreadthFirstSearch
+
+end BreadthFirstSearch
